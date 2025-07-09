db370a6e35a476ec763ad13461a8417c8913fb13
Harry Newman <h.newman2005@outlook.com>
2025-07-09 10:29:51 +0100

    week3 begin

---
+++ week3/sort.cpp
@@ -0,0 +1,9 @@
+/* This was originally a task wherein the student had to run 3 binary files and determine what sort
+ * they were running as they were acting on different types of data (sorted list of 50000 elements,
+ * unsorted list of 1000 elements etc). The 3 possible sorts were: Bubble, Selection and Merge */
+
+void bubbleSort() {}
+
+void selectionSort() {}
+
+void mergeSort() {}
