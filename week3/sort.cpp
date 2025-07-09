5eb572b2053cac04cc5fd5a6f5b133b274d9db7d
Harry Newman <h.newman2005@outlook.com>
2025-07-09 16:18:20 +0100

    Added vector for storing data

--- week3/sort.cpp
+++ week3/sort.cpp
@@ -1,6 +1,23 @@
-/* This was originally a task wherein the student had to run 3 binary files and determine what sort
- * they were running as they were acting on different types of data (sorted list of 50000 elements,
- * unsorted list of 1000 elements etc). The 3 possible sorts were: Bubble, Selection and Merge */
+/* This was originally a task wherein the student had to run 3 binary files and
+ * determine what sort they were running as they were acting on different types
+ * of data (sorted list of 50000 elements, unsorted list of 1000 elements etc).
+ * The 3 possible sorts were: Bubble, Selection and Merge */
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main(void) {
+  size_t num;
+
+  std::ifstream data("unsorted50000.txt");
+
+  std::vector<int> dataVect;
+
+  while (data >> num) {
+    dataVect.push_back((num));
+  }
+}
 
 void bubbleSort() {}
 
