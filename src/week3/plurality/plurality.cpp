81383d3eb6f0e06b9b07768df55c13ee7357f989
Harry Newman <h.newman2005@outlook.com>
2025-07-10 15:13:57 +0100

    Main function (error checking and voting)

--- src/week3/plurality/plurality.cpp
+++ src/week3/plurality/plurality.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -20,3 +22,39 @@ class Candidate {
 };
 
 std::vector<Candidate> candidates;
+
+bool vote(std::string name);
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage: plurality [candidate ...]\n";
+    return 1;
+  }
+
+  if (candidates.size() > MAX) {
+    std::cout << "Maximum number of candidates is " << MAX << std::endl;
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    Candidate newCand;
+
+    newCand.setName(argv[i]);
+    candidates.push_back(newCand);
+  }
+
+  int voterCount;
+  std::cout << "Number of voters:";
+  std::cin >> voterCount;
+
+  for (int i = 0; i < voterCount; ++i) {
+    std::string name;
+    std::cout << "Vote: ";
+    std::cin >> name;
+
+    if (!vote(name)) {
+      std::cout << "Invalid vote\n";
+    }
+  }
+}
+
+bool vote(std::string name) {}
