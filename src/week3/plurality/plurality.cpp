10022a2278d50ac0566cb6e06bb3811e5a045101
Harry Newman <h.newman2005@outlook.com>
2025-07-10 14:57:44 +0100

    Added initialisation candidate methods and a vector to store candidates

--- src/week3/plurality/plurality.cpp
+++ src/week3/plurality/plurality.cpp
@@ -1,7 +1,22 @@
 #include <string>
+#include <vector>
+
+#define MAX 9
 
 class Candidate {
  private:
   std::string name;
   int votes;
+
+ public:
+  Candidate() : name(""), votes(0) {}
+  Candidate(std::string name) : name(name), votes(0) {}
+
+  std::string getName() const { return name; }
+  void setName(std::string name) { this->name = name; }
+
+  int getVotes() const { return votes; }
+  void incrementVotes() { ++votes; }
 };
+
+std::vector<Candidate> candidates;
