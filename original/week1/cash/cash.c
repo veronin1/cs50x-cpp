caf0b2777d4ad2342031ef0b925acb5743f8b461
Harry Newman <h.newman2005@outlook.com>
2025-07-10 13:50:13 +0100

    Moved to respective folder

--- original/week1/cash/cash.c
+++ original/week1/cash/cash.c
@@ -6,38 +6,38 @@ int get_user_change_owed();
 int calculate_change(int change);
 
 int main(void) {
-    int change = get_user_change_owed();
-    int coins_used = calculate_change(change);
-    printf("%d\n", coins_used);
-    return 0;
+  int change = get_user_change_owed();
+  int coins_used = calculate_change(change);
+  printf("%d\n", coins_used);
+  return 0;
 }
 
 int get_user_change_owed() {
-    int change_owed;
-    do {
-        change_owed = get_int("Change owed (cents): ");
-    } while (change_owed < 0);
-    return change_owed;
+  int change_owed;
+  do {
+    change_owed = get_int("Change owed (cents): ");
+  } while (change_owed < 0);
+  return change_owed;
 }
 
 int calculate_change(int change) {
-    int coins_used = 0;
-    while (change >= 25) {
-        coins_used++;
-        change = change - 25;
-    }
-    while (change >= 10) {
-        coins_used++;
-        change = change - 10;
-    }
-    while (change >= 5) {
-        coins_used++;
-        change = change - 5;
-    }
-    while (change >= 1) {
-        coins_used++;
-        change = change - 1;
-    }
+  int coins_used = 0;
+  while (change >= 25) {
+    coins_used++;
+    change = change - 25;
+  }
+  while (change >= 10) {
+    coins_used++;
+    change = change - 10;
+  }
+  while (change >= 5) {
+    coins_used++;
+    change = change - 5;
+  }
+  while (change >= 1) {
+    coins_used++;
+    change = change - 1;
+  }
 
-    return coins_used;
+  return coins_used;
 }
