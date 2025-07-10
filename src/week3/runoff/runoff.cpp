6abb924058ca523a4b2fa29acbb4c0ec2cd3365e
Harry Newman <h.newman2005@outlook.com>
2025-07-10 16:14:49 +0100

    Class, constructors, vector

--- src/week3/runoff/runoff.cpp
+++ src/week3/runoff/runoff.cpp
@@ -1,11 +1,29 @@
 #include <string>
+#include <vector>
 
-#define MAX_VOTERS 100
-#define MAX_CANDIDATES 9
+constexpr int MAX_VOTERS = 100;
+constexpr int MAX_CANDIDATES = 9;
 
 class Candidate {
  private:
-  bool eliminated;
-  int votes;
   std::string name;
+  size_t votes;
+  bool eliminated;
+
+ public:
+  Candidate() : name(""), votes(0), eliminated(false) {}
+  Candidate(const std::string &name)
+      : name(name), votes(0), eliminated(false) {}
+
+  std::string getName() const { return name; }
+  void setName(const std ::string &name) { this->name = name; }
+  void resetVotes() { votes = 0; }
+
+  size_t getVotes() const { return votes; }
+  void incrementVotes() { ++votes; }
+
+  bool isEliminated() const { return eliminated; }
+  void setEliminated(bool status) { eliminated = status; };
 };
+
+std::vector<Candidate> candidates;
